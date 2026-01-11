98eceb80d96871decc6fce9ebe9ac6c358df5533
Huynh Thien Le <itcsiu23036@student.hcmiu.edu.vn>
2026-01-11 21:09:04 +0700

    feat: configure application properties for MySQL database and update dependencies

--- src/main/java/org/example/scrapingtest/ScrapingTestApplication.java
+++ src/main/java/org/example/scrapingtest/ScrapingTestApplication.java
@@ -2,11 +2,13 @@ package org.example.scrapingtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+
 @EnableAsync
-public class ScrapingTestApplication {
+@SpringBootApplication
+public class ScrapingTestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScrapingTestApplication.class, args);
